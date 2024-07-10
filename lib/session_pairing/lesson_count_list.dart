64231ece8f207faeaafa497a9db00acc2f5440ee
thomasfischersm <thomas@playposse.com>
2024-07-10 16:29:55 -0400

    Little debugging stuff.
    Fix but where the lesson counts have different length, e.g. 3, 1 vs 3, 1, 1.

--- lib/session_pairing/lesson_count_list.dart
+++ lib/session_pairing/lesson_count_list.dart
@@ -5,6 +5,14 @@ import 'package:social_learning/session_pairing/paired_session.dart';
 
 /// A holder for all the lesson counts of a pairing. This makes it easier to
 /// compare which offers the rarest lessons.
+///
+/// Later added note: I believe the counts are a list of numbers,
+/// e.g 4, 3, 3, 1. They are how often a particular lesson has been graduated
+/// from. We don't care about the particular lesson, but we care about ordering
+/// the "rarest" lessons.
+///
+/// E.g. 3, 1 vs 2, 2 both has the same amount of graduations. However, 2, 2 is
+/// preferred because it means that all lessons are known by at least 2 people.
 class LessonCountList implements Comparable<LessonCountList> {
   PairedSession pairedSession;
   List<LessonCountComparable> counts = [];
@@ -13,7 +21,7 @@ class LessonCountList implements Comparable<LessonCountList> {
       Map<Lesson, int> graduatedLessonCounts) {
     Map<int, LessonCountComparable> graduatedCountToComparable = {};
     for (Lesson lesson in activeLessons.toSet()) {
-      var graduatedLessonCount = graduatedLessonCounts[lesson] ?? 0;
+      int graduatedLessonCount = graduatedLessonCounts[lesson] ?? 0;
       if (graduatedCountToComparable.containsKey(graduatedLessonCounts)) {
         LessonCountComparable comparable =
         graduatedCountToComparable[graduatedLessonCount]!;
@@ -29,12 +37,14 @@ class LessonCountList implements Comparable<LessonCountList> {
 
   @override
   int compareTo(LessonCountList other) {
-    for (int i = 0; i < counts.length; i++) {
+    for (int i = 0; i < counts.length && i < other.counts.length; i++) {
       if (counts[i].compareTo(other.counts[i]) != 0) {
         return counts[i].compareTo(other.counts[i]);
       }
     }
 
-    return 0;
+    // If one counts has an additional lesson, that's better.
+    // Example: 3, 1, 1 is better than 3, 1.
+    return counts.length.compareTo(other.counts.length);
   }
 }
