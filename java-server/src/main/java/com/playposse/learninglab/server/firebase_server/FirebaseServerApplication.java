40351ae8d5cc8287b30978b30524178b4392c45f
thomasfischersm <thomas@playposse.com>
2025-05-05 15:59:37 -0400

    Added Hello World for testing.

--- java-server/src/main/java/com/playposse/learninglab/server/firebase_server/FirebaseServerApplication.java
+++ java-server/src/main/java/com/playposse/learninglab/server/firebase_server/FirebaseServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirebaseServerApplication {
 
 	public static void main(String[] args) {
+		// Test
 		SpringApplication.run(FirebaseServerApplication.class, args);
 	}
 
---
+++ java-server/src/main/java/com/playposse/learninglab/server/firebase_server/HelloController.java
@@ -0,0 +1,13 @@
+package com.playposse.learninglab.server.firebase_server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Learning Lab server!";
+    }
+}
