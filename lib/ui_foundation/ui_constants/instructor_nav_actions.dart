1e6991f7c109cf923d63cb0adfde1c9bb5c3d1dd
thomasfischersm <thomas@playposse.com>
2025-08-15 18:38:09 -0400

    Remove the course generator and instructor dashboard navigation icons because they are no longer needed.

--- lib/ui_foundation/ui_constants/instructor_nav_actions.dart
+++ lib/ui_foundation/ui_constants/instructor_nav_actions.dart
@@ -5,17 +5,6 @@ class InstructorNavActions {
   static List<IconButton> createActions(BuildContext context) {
     return [
       IconButton(
-        icon: Icon(Icons.bar_chart),
-        tooltip: 'Instructor Dashboard',
-        onPressed: () =>
-            NavigationEnum.instructorDashBoard.navigateClean(context),
-      ),
-      IconButton(
-        icon: const Icon(Icons.auto_stories),
-        tooltip: 'Generate Curriculum',
-        onPressed: () => NavigationEnum.courseGeneration.navigateClean(context),
-      ),
-      IconButton(
         icon: const Icon(Icons.account_tree_outlined),
         tooltip: 'Course Designer', // Updated tooltip
         onPressed: () =>
