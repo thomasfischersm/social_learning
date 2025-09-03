f010358195095ee1a6e24572ab55c4dd3057c002
thomasfischersm <thomas@playposse.com>
2025-09-02 23:21:22 -0400

    Try to improve the logging to only output the messages.

--- java-server/t.ps1
+++ java-server/t.ps1
@@ -1,4 +1,5 @@
 # t.ps1 - Tail logs for Cloud Run service 'learning-lab-server'
 gcloud beta logging tail `
   'resource.type="cloud_run_revision" AND resource.labels.service_name="learning-lab-server"' `
-  --project="social-learning-32741"
+  --project="social-learning-32741" `
+  --format="table(timestamp,severity,textPayload)"
