7a564136e6b73a98006a041114f24bb71a491087
thomasfischersm <thomas@playposse.com>
2025-09-26 17:25:36 -0400

    Limit instructor top nav to dashboard

--- lib/ui_foundation/instructor_dashboard_page.dart
+++ lib/ui_foundation/instructor_dashboard_page.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:social_learning/state/library_state.dart';
 import 'package:social_learning/ui_foundation/helper_widgets/bottom_bar_v2.dart';
-import 'package:social_learning/ui_foundation/helper_widgets/general/learning_lab_app_bar.dart';
+import 'package:social_learning/ui_foundation/helper_widgets/instructor_dashboard/instructor_dashboard_app_bar.dart';
 import 'package:social_learning/ui_foundation/helper_widgets/instructor_dashboard/instructor_dashboard_roster_widget.dart';
 import 'package:social_learning/ui_foundation/helper_widgets/instructor_dashboard/instructor_dashboard_summary_widget.dart';
 import 'package:social_learning/ui_foundation/ui_constants/custom_ui_constants.dart';
 import 'package:social_learning/ui_foundation/ui_constants/custom_text_styles.dart';
+import 'package:social_learning/ui_foundation/ui_constants/navigation_enum.dart';
 
 class InstructorDashboardPage extends StatefulWidget {
   const InstructorDashboardPage({super.key});
@@ -19,7 +20,9 @@ class InstructorDashboardState extends State<InstructorDashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const LearningLabAppBar(),
+      appBar: const InstructorDashboardAppBar(
+        currentNav: NavigationEnum.instructorDashBoard,
+      ),
       bottomNavigationBar: BottomBarV2.build(context),
       body: Align(
         alignment: Alignment.topCenter,
